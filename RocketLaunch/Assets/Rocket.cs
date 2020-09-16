624bc1fc750a6e146f5d55085d4cfceb0d5b128f
Raghav <14raghav.kulkarni@gmail.com>
2020-09-16 13:01:03 +0530

    add script
    Script for Rotation and translation of Rocket

--- RocketLaunch/Assets/Rocket.cs
+++ RocketLaunch/Assets/Rocket.cs
@@ -5,32 +5,52 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float trustRotate = 250.0f;
+    [SerializeField] float trustMovment = 50.0f;
+
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        MovmentOfRocket();
+        RotationOfRocket();
     }
 
-    private void ProcessInput()
+    private void MovmentOfRocket()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * trustMovment);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
-        if(Input.GetKey(KeyCode.A))
+        else
         {
-            Debug.Log("Left Move Pressed");
+            audioSource.Stop();
+        }
+    }
+    private void RotationOfRocket()
+    {
+        float rotationAngle = trustRotate * Time.deltaTime;
+        rigidBody.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationAngle);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Right Move Pressed");
+            transform.Rotate(-Vector3.forward * rotationAngle);
         }
+        rigidBody.freezeRotation = false;
     }
 }
