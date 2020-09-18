59ab13559a483f793b4b988e759c1eea760a3310
Raghav <14raghav.kulkarni@gmail.com>
2020-09-18 13:02:57 +0530

    Middle Level commit
    Includes,
    Levels ,
    Scene Managment,
    oscillators

---
+++ RocketLaunch/Assets/Oscillator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movmentVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float periodForCycle = 2f;
+
+    float movmentFactor; //0 is not move , 1 is full move
+    Vector3 startingPosition;
+    const float tau = 2 * Mathf.PI;
+
+    void Start()
+    {
+        startingPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(periodForCycle <= Mathf.Epsilon) { return; } // period is never be zero
+        float cycle = Time.time / periodForCycle; //It return cycle as per frame rate.
+  
+        float rawSinWave = Mathf.Sin(tau * cycle); //Caluclate sin wave between -1 to +1
+                                                   //but we want 0 to 1 .....for this we add below logic
+        movmentFactor = rawSinWave / 2f + 0.5f;
+        
+        //code for how much obstacles moves between range 0 and 1
+        Vector3 offset = movmentFactor * movmentVector;
+        this.transform.position = startingPosition + offset;
+    }
+}
--- RocketLaunch/Assets/Rocket.cs
+++ RocketLaunch/Assets/Rocket.cs
@@ -2,11 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float trustRotate = 250.0f;
     [SerializeField] float trustMovment = 50.0f;
+
+    [SerializeField] AudioClip rocketSound;
+    [SerializeField] AudioClip newLevelSound;
+    [SerializeField] AudioClip deadSound;
+
+    [SerializeField] ParticleSystem rocketParticles;
+    [SerializeField] ParticleSystem newLevelParticles;
+    [SerializeField] ParticleSystem deadParticles;
+
+    enum State {Alive , Dead , Trasending};//Transending : Moving Next Level
+    State state;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
@@ -15,42 +27,87 @@ public class Rocket : MonoBehaviour
     {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        state = State.Alive;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MovmentOfRocket();
-        RotationOfRocket();
+        if(state == State.Alive)
+        {
+            MovmentOfRocket();
+            RotationOfRocket();
+        }
     }
 
     private void MovmentOfRocket()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * trustMovment);
-            if(!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyMovement();
         }
         else
         {
             audioSource.Stop();
+            rocketParticles.Stop();
         }
     }
+
+    private void ApplyMovement()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * trustMovment);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(rocketSound);
+            rocketParticles.Play();
+        }
+    }
+
     private void RotationOfRocket()
     {
         float rotationAngle = trustRotate * Time.deltaTime;
         rigidBody.freezeRotation = true;
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(Vector3.forward * rotationAngle);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(-Vector3.forward * rotationAngle);
         }
         rigidBody.freezeRotation = false;
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (state != State.Alive) { return; }//This code for ..when player is deied then it do not go to other collision messages .hence below loop is stop 
+        //due to this code block(State not equal alive means dead)  
+      
+        if(collision.gameObject.tag == "Finish")
+        {
+            state = State.Trasending;;
+            audioSource.PlayOneShot(newLevelSound);
+            newLevelParticles.Play();
+            Invoke("LoadNextScene", 2f); //call function
+        }
+        else if(collision.gameObject.tag == "Unsafe")
+        {
+            state = State.Dead;
+            audioSource.PlayOneShot(deadSound);
+            deadParticles.Play();
+            Invoke("LoadBeginingScene", 2f); //call function
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene("Level-2");
+    }
+
+    private void LoadBeginingScene()
+    {
+        SceneManager.LoadScene("Level-1");
+    }
+
 }
